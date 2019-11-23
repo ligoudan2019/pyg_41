909e2a9dbd33e372224eb49d392c6cf700d4de31
李二狗 <333333@qq.com>
2019-11-23 11:53:49 +0800

    实现了全选和点选

--- js/cart.js
+++ js/cart.js
@@ -48,4 +48,19 @@ $(function () {
     $('.cart-header').show(); // 显示表头
     $('.total-of').show(); // 显示用于结算的div
   }
+
+  //第二个功能 全选和点选
+  $('.pick-all').on('click',function(){
+    let status = $(this).prop('checked');
+    $('.item-ck').prop('checked',status);
+    $('.pick-all').prop('checked',status);
+  })
+
+  // 点选 - 所有的点选的checkbox都是动态生成的，推荐使用委托离开实现
+  $('.item-list').on('click','.item-ck',function(){
+    // 如果勾选的个数和总个数一致 = 全选
+    let ckall = $('.item-ck').length === $('.item-ck:checked').length;
+    // 设置全选的状态和ckall一致就行
+    $('.pick-all').prop('checked',ckall);
+  })
 });
